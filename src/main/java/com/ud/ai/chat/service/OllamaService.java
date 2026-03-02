f51841509f3ec46ad77a0c86143644436b96c9d1
udithnalaka <udithnalaka@yahoo.com.au>
2026-03-02 16:38:25 +1000

    async call endpoint to olloma

--- src/main/java/com/ud/ai/chat/service/OllamaService.java
+++ src/main/java/com/ud/ai/chat/service/OllamaService.java
@@ -6,6 +6,7 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 
 @Service
 public class OllamaService {
@@ -28,5 +29,21 @@ public class OllamaService {
         log.info("Ollama response completed using the AI model: {}.", chatResponse.getMetadata().getModel());
 
         return chatResponse.getResult().getOutput().getText();
+    }
+
+    /**
+     * chatClient using stream() for async call to the ollama LLM.
+     * for a large response, the user will be getting the responses one by one rather than waiting for the complete response
+     * from Ollama before sending the response to the user.
+     * @param message
+     * @return Flux<String>
+     */
+    public Flux<String> getSuggestions(String message) {
+
+        return chatClient
+                .prompt(message)
+                .stream()
+                .content();
+
     }
 }
