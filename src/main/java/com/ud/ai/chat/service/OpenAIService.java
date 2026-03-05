2c58e22dec0620c1788e95eef9fd87fcccc60098
udithnalaka <udithnalaka@yahoo.com.au>
2026-03-05 14:55:08 +1000

    using promopt with system instruction to call open ai model

--- src/main/java/com/ud/ai/chat/service/OpenAIService.java
+++ src/main/java/com/ud/ai/chat/service/OpenAIService.java
@@ -15,7 +15,6 @@ public class OpenAIService {
         this.chatClient = ChatClient.create(chatModel);
     }
 
-
     public String getAnswer(String message) {
 
         //simple call to the model
@@ -23,7 +22,6 @@ public class OpenAIService {
                 .prompt(message)
                 .call()
                 .content();
-
     }
 
     /**
@@ -38,6 +36,66 @@ public class OpenAIService {
         return chatClient
                 .prompt(message)
                 .stream()
+                .content();
+    }
+
+    /**
+     * ChatClient using a system instruction message to only respond to the banking related questions.
+     * Or else, send a default message back.
+     *
+     * @param message to ask from the agent
+     * @return String
+     */
+    public String getBankingChatResponse(String message) {
+
+        var systemInstructions = """
+                You are a customer service assistant for MyBank.
+                You can ONLY discuss:
+                - Accout Balance and transactions
+                - Branch location and hours
+                - General banking services
+                
+                If asked anything else, respond: "I can only help with banking related questions."
+                """;
+
+        return chatClient.prompt()
+                .user(message)
+                .system(systemInstructions)
+                .call()
+                .content();
+    }
+
+    public String createBlogPost(String topic) {
+
+        // use it as a guide or a restriction to the model's behaviour.
+        var systemInstruction = """
+                Blog Post Generation Guidelines:
+                
+                1. Length & Purpose: Generate a 200 word blog post that informs and engage school kids as audience.
+                
+                2. Structure:
+                    - Introduction: Hook readers and establish the topic's relevance.
+                    - Body: Develop a main point with supporting evidence and example.
+                    - Conclusion: summarize key takeaways.
+                    
+                3. Content requirements:
+                    - Include real world examples.
+                    - explain clearly for non expert audience.
+                    
+                4. Tone & Style: 
+                    - Write in an informational yet conversational voice.
+                    - use simple english language. 
+                    
+                5. Response format: Delivery complete ready to publish post with a suggested title.
+                """;
+
+        return chatClient.prompt()
+                .user(u -> {
+                    u.text("Write me a blog post about {topic}");
+                    u.param("topic", topic);
+                })
+                .system(systemInstruction)
+                .call()
                 .content();
     }
 }
